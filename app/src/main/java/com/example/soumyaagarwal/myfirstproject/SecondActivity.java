0c22782c78e3fe3caa24d990382e28187ddcdf0e
Mohammed <mhmdtajeldin@gmail.com>
2022-08-15 06:45:10 +0200

    modify new resources

--- app/src/main/java/com/example/soumyaagarwal/myfirstproject/SecondActivity.java
+++ app/src/main/java/com/example/soumyaagarwal/myfirstproject/SecondActivity.java
@@ -1,2 +1,35 @@
-package com.example.soumyaagarwal.myfirstproject;public class SecondActivity {
+package com.example.soumyaagarwal.myfirstproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    TextView textreceived;
+    Button button2;
+    String text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        text = getIntent().getExtras().getString("Text");
+        textreceived = (TextView)findViewById(R.id.textreceived);
+        textreceived.setText(text);
+
+        button2 = (Button)findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this,FirstActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }

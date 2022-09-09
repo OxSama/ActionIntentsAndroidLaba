5d9a5e0f1eb7ac8eea32d28e690ec411c1ec7985
Mohammed <mhmdtajeldin@gmail.com>
2022-09-09 22:13:10 +0200

    modifications in the lab

--- app/src/main/java/com/example/soumyaagarwal/myfirstproject/MainActivity.java
+++ app/src/main/java/com/example/soumyaagarwal/myfirstproject/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.soumyaagarwal.myfirstproject;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,7 +19,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_first);
 
         clickme = (Button)findViewById(R.id.button);
-        text = (EditText)findViewById(R.id.text);
+//        text = (EditText)findViewById(R.id.text);
         clickme.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
